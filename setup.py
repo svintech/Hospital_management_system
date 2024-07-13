5374c0ca8a53ae8ff78f1615e31308ac59bd2580
Lodrick Kibochi <149433066+Lodrick1@users.noreply.github.com>
2024-07-13 15:21:19 +0300

    app.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HospitalManagementSystem',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='Tushar',
+    author_email='tusharborole@ourmail.com',
+    description='Simple Hospital Management System Project'
+)
